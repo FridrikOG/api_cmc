22a481d1619729cd36f7952ec5329d4411cbc3f0
Friðrik Ö. Gunnarsson <36244964+FridrikOG@users.noreply.github.com>
2020-01-22 14:53:05 +0000

    Works

---
+++ portfolio_manager.py
@@ -0,0 +1,25 @@
+
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+parameters = {
+  'start':'1',
+  'limit':'5000',
+  'convert':'USD'
+}
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': '85b403c5-f580-4b25-87dc-a88de76e0bf9',
+}
+
+session = Session()
+session.headers.update(headers)
+
+try:
+  response = session.get(url, params=parameters)
+  data = json.loads(response.text)
+  print(data)
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+  print(e)
